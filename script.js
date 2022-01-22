713da2c86aae9e8baac0aaa7b6adc3d536110b62
Danielle Byrne <DanielleByrne421@gmail.com>
2022-01-21 23:49:28 -0800

    removed a console.log and the id code because it's not necessary for adding an item to the list

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-let id = 0;
 
 document.getElementById("add").addEventListener("click", () => {
 event.preventDefault()
   let table = document.getElementById("grocery-table");
   let row = table.insertRow(1);
-  row.setAttribute("id", `item-${id}`);
   row.insertCell(0).innerHTML = document.getElementById("newItem").value;
   row.insertCell(1).innerHTML = document.getElementById("quantity").value;
-  console.log(document.getElementById("quantity").value)
   document.getElementById("newItem").value = "";
   document.getElementById("quantity").value = "";
 });
